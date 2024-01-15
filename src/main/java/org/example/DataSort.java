43825536f52e4a8ac569d1e30740b86efc0058ff
Jonman <Jonman@yandex.ru>
2024-01-16 00:25:14 +0700

    simple work with sort one file and short stat

--- src/main/java/org/example/DataSort.java
+++ src/main/java/org/example/DataSort.java
@@ -1,29 +1,86 @@
 package org.example;
 
 import java.io.*;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class DataSort {
     public static void openFile(String filename) throws FileNotFoundException {
         try (Scanner scanner = new Scanner(new FileInputStream(filename))) {
-            while (scanner.hasNextLine()){
+            while (scanner.hasNext()) {
                 if (scanner.hasNextInt()) {
                     try {
                         FileWriter writer = new FileWriter("example.txt", true);
-                        int  a = scanner.nextInt();
-                        writer.write(Integer.toString(a));
+                        int intNumber = scanner.nextInt();
+                        writer.write(Integer.toString(intNumber));
+                        writer.write("\n");
                         writer.close();
                     } catch (IOException e) {
                         System.out.println("Ошибка при записи в файл");
                     }
-                }else{
-                       break;
+                } else if (scanner.hasNextDouble()) {
+                    try {
+                        FileWriter writer = new FileWriter("example1.txt", true);
+                        double floatNumber = scanner.nextDouble();
+                        writer.write(Double.toString(floatNumber));
+                        writer.write("\n");
+                        writer.close();
+                    } catch (IOException e) {
+                        System.out.println("Ошибка при записи в файл");
+                    }
+                } else if (scanner.hasNextLine()) {
+                    try {
+                        FileWriter writer = new FileWriter("example2.txt", true);
+                        String line = scanner.nextLine();
+                        writer.write(line);
+                        writer.write("\r");
+                        writer.close();
+                    } catch (IOException e) {
+                        System.out.println("Ошибка при записи в файл");
+                    }
+                } else {
+                    break;
                 }
             }
         }
     }
 
+    public static int elementsCount;
+
+    public static void shortStat(String[] filename) {
+        for (String string : filename) {
+            elementsCount = 0;
+            try (Scanner scanner = new Scanner(new FileInputStream(string))) {
+                if (scanner.hasNextInt()) {
+                    while (scanner.hasNext()) {
+                        scanner.nextInt();
+                        elementsCount++;
+                    }
+                } else if (scanner.hasNextDouble()) {
+                    while (scanner.hasNext()) {
+                        scanner.nextDouble();
+                        elementsCount++;
+                    }
+                } else if (scanner.hasNextLine()) {
+                    while (scanner.hasNext()) {
+                        scanner.nextLine();
+                        elementsCount++;
+                    }
+                } else {
+                    break;
+                }
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Количество записанных элементов в файле " + string + " равно: " + elementsCount);
+        }
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
+        Locale.setDefault(new Locale("en", "US"));
         openFile("src/input.txt");
+        String[] strings = new String[]{"example.txt", "example1.txt", "example2.txt"};
+        shortStat(strings);
     }
 }
