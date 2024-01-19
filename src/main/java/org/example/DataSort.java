11ac29c42af877ee01e95204594a2eed8a92e81e
Jonman <Jonman@yandex.ru>
2024-01-19 23:12:24 +0700

    update args 2.0

--- src/main/java/org/example/DataSort.java
+++ src/main/java/org/example/DataSort.java
@@ -5,53 +5,76 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class DataSort {
-    public static void openFile(String filename) {
-        try (Scanner scanner = new Scanner(new FileInputStream(filename))) {
+    public static void openFile(String inputFilename, String[] outputFilename, String path, boolean a) {
+        for (String string : outputFilename) {
+            File file = new File(string);
+            if (file.exists() && !a) {
+                System.out.println("Файл найден");
+                boolean delete = file.delete();
+
+                if (delete) {
+                    System.out.println("Файл удален");
+                }
+            }
+        }
+
+        File mkdirs = new File(path);
+        boolean mkdirs1 = mkdirs.mkdirs();
+
+        try (Scanner scanner = new Scanner(new FileInputStream(inputFilename))) {
             while (scanner.hasNext()) {
                 if (scanner.hasNextInt()) {
                     try {
-                        FileWriter writer = new FileWriter("integers.txt", true);
+                        FileWriter writer = new FileWriter(path + outputFilename[0], true);
+
                         int intNumber = scanner.nextInt();
                         writer.write(Integer.toString(intNumber));
                         writer.write("\n");
                         writer.close();
                     } catch (IOException e) {
                         System.out.println("Ошибка при записи в файл");
+                        break;
                     }
                 } else if (scanner.hasNextDouble()) {
                     try {
-                        FileWriter writer = new FileWriter("floats.txt", true);
+                        FileWriter writer = new FileWriter(path + outputFilename[1], true);
                         double floatNumber = scanner.nextDouble();
                         writer.write(Double.toString(floatNumber));
                         writer.write("\n");
                         writer.close();
                     } catch (IOException e) {
                         System.out.println("Ошибка при записи в файл");
+                        break;
                     }
                 } else if (scanner.hasNextLine()) {
                     try {
-                        FileWriter writer = new FileWriter("strings.txt", true);
+                        FileWriter writer = new FileWriter(path + outputFilename[2], true);
                         String line = scanner.nextLine();
 
-                        if(!line.isEmpty()){
+                        if (!line.isEmpty()) {
                             writer.write(line + "\n");
                         }
 
                         writer.close();
                     } catch (IOException e) {
                         System.out.println("Ошибка при записи в файл");
+                        break;
                     }
                 } else {
                     System.out.println("Ошибка при считывании данных с файла");
                     break;
                 }
             }
-        } catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("Не удается открыть файл");
         }
     }
 
-    public static void stat(String[] filename, String statType) {
+    public static void stat(String[] filename, String path, int statType) {
+        for (int i = 0; i < filename.length; i++) {
+            filename[i] = path + filename[i];
+        }
+
         for (String string : filename) {
             int elementsCount = 0;
             double average = 0;
@@ -66,8 +89,8 @@ public class DataSort {
             int shortString = 0;
             int longString = 0;
 
-            boolean intFile = false;
-            boolean doubleFile = false;
+            boolean isCreateIntFile = false;
+            boolean isCreateDoubleFile = false;
 
             try (Scanner scanner = new Scanner(new FileInputStream(string))) {
                 if (scanner.hasNextInt()) {
@@ -91,7 +114,7 @@ public class DataSort {
                     }
 
                     average = (double) intSum / elementsCount;
-                    intFile = true;
+                    isCreateIntFile = true;
                 } else if (scanner.hasNextDouble()) {
                     double doubleNumber = scanner.nextDouble();
                     doubleMax = doubleNumber;
@@ -113,7 +136,7 @@ public class DataSort {
                     }
 
                     average = doubleSum / elementsCount;
-                    doubleFile = true;
+                    isCreateDoubleFile = true;
                 } else if (scanner.hasNextLine()) {
                     String stringWord = scanner.nextLine();
                     elementsCount++;
@@ -139,25 +162,25 @@ public class DataSort {
                 throw new RuntimeException(e);
             }
 
-            if (statType.equals("-s")) {
+            if (statType == 2) {
                 System.out.println("Количество записанных элементов в файле " + string + " равно: " + elementsCount + ".\n");
-            } else if (statType.equals("-f") && intFile) {
+            } else if (statType == 1 && isCreateIntFile) {
                 System.out.println("Количество записанных элементов в файле " + string + " равно: " + elementsCount + ".");
                 System.out.println("Сумма записанных элементов в файле " + string + " равна: " + intSum + ".");
                 System.out.println("Среднее значение записанных элементов в файле " + string + " равна: " + average + ".");
                 System.out.println("Минимальное значение записанных элементов в файле " + string + " равна: " + intMin + ".");
                 System.out.println("Максимальное значение записанных элементов в файле " + string + " равна: " + intMax + ".\n");
-            } else if (statType.equals("-f") && doubleFile) {
+            } else if (statType == 1 && isCreateDoubleFile) {
                 System.out.println("Количество записанных элементов в файле " + string + " равно: " + elementsCount + ".");
                 System.out.println("Сумма записанных элементов в файле " + string + " равна: " + doubleSum + ".");
                 System.out.println("Среднее значение записанных элементов в файле " + string + " равна: " + average + ".");
                 System.out.println("Минимальное значение записанных элементов в файле " + string + " равна: " + doubleMin + ".");
                 System.out.println("Максимальное значение записанных элементов в файле " + string + " равна: " + doubleMax + ".\n");
-            } else if (statType.equals("-f")) {
+            } else if (statType == 1) {
                 System.out.println("Количество записанных строк в файле " + string + " равно: " + elementsCount + ".");
-                System.out.println("Максимальная строка в файле " + string + " содержит: " + longString + "символов.");
-                System.out.println("Минимальная строка в файле " + string + " содержит: " + shortString + "символов.\n");
-            } else {
+                System.out.println("Максимальная строка в файле " + string + " содержит: " + longString + " символов.");
+                System.out.println("Минимальная строка в файле " + string + " содержит: " + shortString + " символов.\n");
+            } else if (statType != 0) {
                 System.out.println("Ошибка статистики в файле " + string + ".\n");
             }
         }
@@ -165,31 +188,38 @@ public class DataSort {
 
     public static void main(String[] args) throws FileNotFoundException {
         Locale.setDefault(new Locale("en", "US")); // Use for dot
-        openFile("src/input.txt");
-        String[] strings = new String[]{"integers.txt", "floats.txt", "strings.txt"};
+        String[] outputFilenames = new String[]{"integers.txt", "floats.txt", "strings.txt"};
+
         String path = "";
-        stat(strings, "-f");
-        stat(strings, "-s");
+
+        int statisticCount = 0;
+        boolean isInputA = false;
 
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
 
-            if(args[i].equals("-o") || args[i].equals("-O")){
+            if (args[i].equals("-o") || args[i].equals("-O")) {
                 System.out.println("Содержит -o");
                 path = args[i + 1];
-            }else if(args[i].equals("-p") || args[i].equals("-P")){
+            } else if (args[i].equals("-p") || args[i].equals("-P")) {
                 System.out.println("Содержит -p");
 
-                for(int j = 0; j < strings.length; j++){
-                    strings[j] = strings[j] + args[i + 1];
+                for (int j = 0; j < outputFilenames.length; j++) {
+                    outputFilenames[j] = args[i + 1] + outputFilenames[j];
                 }
-            }else if(args[i].equals("-f") || args[i].equals("-F")){
+            } else if (args[i].equals("-f") || args[i].equals("-F")) {
                 System.out.println("Содержит -f");
-            }else if(args[i].equals("-s") || args[i].equals("-S")){
+                statisticCount = 1;
+            } else if (args[i].equals("-s") || args[i].equals("-S")) {
                 System.out.println("Содержит -s");
-            }else if(args[i].equals("-a") || args[i].equals("-A")){
+                statisticCount = 2;
+            } else if (args[i].equals("-a") || args[i].equals("-A")) {
                 System.out.println("Содержит -a");
+                isInputA = true;
             }
         }
+
+        openFile("input.txt", outputFilenames, path, isInputA);
+        stat(outputFilenames, path, statisticCount);
     }
 }
